17eff4d3ac221a9935c2c8ae4a80e9711d894840
An-Annoying-Cat <57719091+An-Annoying-Cat@users.noreply.github.com>
2023-01-05 02:29:57 -0600

    chaff rework

--- scripts/entities/enemies/chaff.lua
+++ scripts/entities/enemies/chaff.lua
@@ -1,6 +1,7 @@
 local Mod = BotB
 local CHAFF = {}
 local Entities = BotB.Enums.Entities
+local Fiend = Mod.FF
 
 function CHAFF:NPCUpdate(npc)
 
@@ -13,18 +14,46 @@ function CHAFF:NPCUpdate(npc)
 	local targetdistance = (targetpos - npc.Position):Length()
 
     --Chaff (Plays the wheeze sound, shoots an attack fly, checks for limit)
-    if npc.Type == Entities.CHAFF.TYPE and npc.Variant == Entities.CHAFF.VARIANT then 
-        if npc.State == 8 then npc.State = 9 sprite:Play("Shoot") end 
+    if npc.Type == Entities.CHAFF.TYPE and npc.Variant == Entities.CHAFF.VARIANT then
+        if npc.State == 8 then npc.State = 9 sprite:Play("Shoot") end
             if npc.State == 9 then
                 if sprite:IsEventTriggered("Shoot") then
-                    --3 flies per chaff max, so player doesn't get overwhelmed
-                    if Isaac.CountEntities(npc, 18, 0) < 3 then
+
+                    local data = npc:GetData()
+                    if not data["BotBChildren"] then --create eternal fly table
+                        data["BotBChildren"] = {}
+                    end
+
+                    if #data["BotBChildren"] < 2 then --spawn eternal flies if none exist
                         sfx:Play(Isaac.GetSoundIdByName("Wheeze"),1,0,false,math.random(75, 85)/100)
-                        local spawnedFly = Isaac.Spawn(18, 0, 0, npc.Position + Vector(5,0):Rotated(targetangle), Vector(0,0), npc)
-                        spawnedFly:ClearEntityFlags(EntityFlag.FLAG_APPEAR)
-                        spawnedFly.Velocity = Vector(6,0):Rotated(targetangle)
-                        spawnedFly.HitPoints = 1
+                        local spawnedEternalFly = Isaac.Spawn(Fiend.FF.DeadFlyOrbital.ID, Fiend.FF.DeadFlyOrbital.Var, 0, npc.Position + Vector(1,0):Rotated(targetangle), Vector(6,0):Rotated(targetangle), npc)
+                        spawnedEternalFly.Parent = npc
+                        npc.Child = spawnedEternalFly
+                        table.insert(data["BotBChildren"], spawnedEternalFly)
+
+                    elseif Isaac.CountEntities(npc, Fiend.FF.DeadFlyOrbital.ID, Fiend.FF.DeadFlyOrbital.Var) >= 2 then
+
+                        for _, ent in ipairs(Isaac.FindInRadius(npc.Position, 20, EntityPartition.ENEMY)) do
+                            if ent.Type == Fiend.FF.DeadFlyOrbital.ID and ent.Variant == Fiend.FF.DeadFlyOrbital.Var then
+                                --if fly is too close then just wheeze
+                                sfx:Play(Isaac.GetSoundIdByName("Wheeze"),0.5,0,false,math.random(100, 125)/100)
+                            return end
+                        end
+
+                        sfx:Play(Isaac.GetSoundIdByName("Wheeze"),1,0,false,math.random(75, 85)/100)
+                        for i = #data["BotBChildren"], 1, -1 do --detach eternal flies
+                            local fly = data["BotBChildren"][i]
+                            fly.Parent = nil
+                            fly:ToNPC():Morph(Fiend.FF.DeadFlyOrbital.ID, Fiend.FF.DeadFlyOrbital.Var, 0, -1)
+                            fly.HitPoints = 1
+                            fly.SpriteScale = Vector(0.5,0.5)
+                            fly.SpawnerEntity = npc
+                        end
                     else
+                        for i = #data["BotBChildren"], 1, -1 do
+                            local fly = data["BotBChildren"][i]
+                            if not fly:Exists() then table.remove(data["BotBChildren"], i) end
+                        end
                         sfx:Play(Isaac.GetSoundIdByName("Wheeze"),0.5,0,false,math.random(100, 125)/100)
                     end
                 end
@@ -57,5 +86,5 @@ function CHAFF:NPCDeathCheck(npc)
 
 end
 
-Mod:AddCallback(ModCallbacks.MC_NPC_UPDATE, CHAFF.NPCUpdate, Isaac.GetEntityTypeByName("Chaff"))
+Mod:AddCallback(ModCallbacks.MC_NPC_UPDATE, CHAFF.NPCUpdate, Entities.CHAFF.TYPE)
 Mod:AddCallback(ModCallbacks.MC_POST_NPC_DEATH, CHAFF.NPCDeathCheck)
