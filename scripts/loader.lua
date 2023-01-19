6917a676222dfad7de8bbd82071ff24522f7eb92
An-Annoying-Cat <57719091+An-Annoying-Cat@users.noreply.github.com>
2023-01-19 12:20:36 -0600

    a little bit of restructuring

--- scripts/loader.lua
+++ scripts/loader.lua
@@ -2,92 +2,92 @@ local Mod = BotB
 
 Mod.LoadScripts({
     --ENTITIES
-    "scripts.entities.drifter",
-    "scripts.entities.golfball",
+    "scripts/entities/drifter",
+    "scripts/entities/golfball",
 
 
     --ENEMIES
-    "scripts.entities.enemies.eternalfly",
+    "scripts/entities/enemies/eternalfly",
 
-    "scripts.entities.enemies.chapter_1.skooter",
-    "scripts.entities.enemies.chapter_1.ping",
-    "scripts.entities.enemies.chapter_1.pong",
-    "scripts.entities.enemies.chapter_1.inchworm",
-    "scripts.entities.enemies.chapter_1.sleazebag",
-    "scripts.entities.enemies.chapter_1.chaff",
-    "scripts.entities.enemies.chapter_1.innie",
-    "scripts.entities.enemies.chapter_1.inniereverse",
-    "scripts.entities.enemies.chapter_1.planecreep",
-    "scripts.entities.enemies.chapter_1.ms_horf",
+    "scripts/entities/enemies/chapter_1/skooter",
+    "scripts/entities/enemies/chapter_1/ping",
+    "scripts/entities/enemies/chapter_1/pong",
+    "scripts/entities/enemies/chapter_1/inchworm",
+    "scripts/entities/enemies/chapter_1/sleazebag",
+    "scripts/entities/enemies/chapter_1/chaff",
+    "scripts/entities/enemies/chapter_1/innie",
+    "scripts/entities/enemies/chapter_1/inniereverse",
+    "scripts/entities/enemies/chapter_1/planecreep",
+    "scripts/entities/enemies/chapter_1/ms_horf",
 
-    "scripts.entities.enemies.chapter_1.5.dross.culo",
+    "scripts/entities/enemies/chapter_1.5/dross/culo",
 
-    "scripts.entities.enemies.chapter_2.tippytap",
-    "scripts.entities.enemies.chapter_2.croast",
-    "scripts.entities.enemies.chapter_2.funguy",
+    "scripts/entities/enemies/chapter_2/tippytap",
+    "scripts/entities/enemies/chapter_2/croast",
+    "scripts/entities/enemies/chapter_2/funguy",
 
-    "scripts.entities.enemies.chapter_2.flooded_caves.hydroknight",
+    "scripts/entities/enemies/chapter_2/flooded_caves/hydroknight",
 
-    "scripts.entities.enemies.chapter_2.5.mines.kettle",
-    "scripts.entities.enemies.chapter_2.5.mines.crockpot",
+    "scripts/entities/enemies/chapter_2.5/mines/kettle",
+    "scripts/entities/enemies/chapter_2.5/mines/crockpot",
 
-    "scripts.entities.enemies.chapter_3.seducer",
-    "scripts.entities.enemies.chapter_3.desirer",
-    "scripts.entities.enemies.chapter_3.palesniffle",
-    "scripts.entities.enemies.chapter_3.puer_puella",
+    "scripts/entities/enemies/chapter_3/seducer",
+    "scripts/entities/enemies/chapter_3/desirer",
+    "scripts/entities/enemies/chapter_3/palesniffle",
+    "scripts/entities/enemies/chapter_3/puer_puella",
 
-    "scripts.entities.enemies.chapter_3.5.kehehan",
-    "scripts.entities.enemies.chapter_3.5.shard",
-    "scripts.entities.bosses.thaumaturge",
+    "scripts/entities/enemies/chapter_3.5/kehehan",
+    "scripts/entities/enemies/chapter_3.5/shard",
+    "scripts/entities/bosses/thaumaturge",
 
-    "scripts.entities.enemies.chapter_3.5.gehenna.mabel",
+    "scripts/entities/enemies/chapter_3.5/gehenna/mabel",
 
-    "scripts.entities.enemies.chapter_4.womb.giblet",
-    "scripts.entities.enemies.chapter_4.womb.gibby",
-    "scripts.entities.enemies.chapter_4.womb.cadre",
+    "scripts/entities/enemies/chapter_4/womb/giblet",
+    "scripts/entities/enemies/chapter_4/womb/gibby",
+    "scripts/entities/enemies/chapter_4/womb/cadre",
 
-    "scripts.entities.enemies.ascent.batso",
+    "scripts/entities/enemies/ascent/batso",
 
 
     --FAMILIARS
-    "scripts.entities.familiars.robobabyzero",
-    "scripts.entities.familiars.onyxmarble",
-    "scripts.entities.familiars.atombombbaby",
-    "scripts.entities.familiars.bhf",
-    "scripts.entities.familiars.faithfulfleet",
+    "scripts/entities/familiars/robobabyzero",
+    "scripts/entities/familiars/onyxmarble",
+    "scripts/entities/familiars/atombombbaby",
+    "scripts/entities/familiars/bhf",
+    "scripts/entities/familiars/faithfulfleet",
 
 
     --ITEMS
-    "scripts.entities.items.alphaarmor",
-    "scripts.entities.items.treemansyndrome",
-    "scripts.entities.items.placeholder_item",
-    "scripts.entities.items.toy_helicopter",
-    "scripts.entities.items.thehunger",
-    "scripts.entities.items.50shades",
-    --"scripts.entities.items.grub",
-    --"scripts.entities.items.mudpie",
-    --"scripts.entities.items.houseplant",
+    "scripts/entities/pickups/items/alphaarmor",
+    "scripts/entities/pickups/items/treemansyndrome",
+    "scripts/entities/pickups/items/placeholder_item",
+    "scripts/entities/pickups/items/toy_helicopter",
+    "scripts/entities/pickups/items/thehunger",
+    "scripts/entities/pickups/items/50shades",
+    --"scripts/entities/items/grub",
+    --"scripts/entities/items/mudpie",
+    --"scripts/entities/items/houseplant",
 
 
     --TRINKETS
-    "scripts.entities.items.trinkets.demoncore",
-    "scripts.entities.items.trinkets.asingleraisin",
-    "scripts.entities.items.trinkets.placeholder_trinket",
+    "scripts/entities/pickups/trinkets/demoncore",
+    "scripts/entities/pickups/trinkets/asingleraisin",
+    "scripts/entities/pickups/trinkets/placeholder_trinket",
 
 
     --PICKUPS
-    "scripts.entities.items.pickups.kickcube",
-    "scripts.entities.items.pickups.jumpcrystal",
+    "scripts/entities/pickups/kickcube",
+    "scripts/entities/pickups/jumpcrystal",
 
 
     --CONSUMABLES
-    --"scripts.entities.consumables.basic",
-    "scripts.entities.consumables.shotgunkingcards",
+    --"scripts/entities/pickups/consumables/basic",
+    "scripts/entities/pickups/consumables/shotgunkingcards",
 
-    "scripts.entities.consumables.mahjongtiles",
+    "scripts/entities/pickups/consumables/mahjongtiles",
 
 
     --PLAYERS
-    "scripts.players.jezebel",
+    "scripts/entities/players/jezebel",
 
 })
