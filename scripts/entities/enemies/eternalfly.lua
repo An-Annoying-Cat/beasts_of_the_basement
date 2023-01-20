739a0d70e9308c7ffba01eee77a692350207cc74
An-Annoying-Cat <57719091+An-Annoying-Cat@users.noreply.github.com>
2023-01-20 15:45:12 -0600

    fixed eternal fly bug

--- scripts/entities/enemies/eternalfly.lua
+++ scripts/entities/enemies/eternalfly.lua
@@ -1,33 +1,24 @@
 local Mod = BotB
-local FiendFolio = Mod.FF
+local ffmod = Mod.FF
 local ETERNAL_FLY = {}
 local Entities = BotB.Enums.Entities
 
 function ETERNAL_FLY:NPCUpdate(npc)
     local d = npc:GetData()
     if npc.Variant == Entities.ETERNAL_FLY_DUMMY.VARIANT then
-        if not d.init then
-            for _, ent in ipairs(Isaac.FindInRadius(npc.Position, 50, EntityPartition.ENEMY)) do
-                --put this in a scheduler because it sometimes bugs
-                local fly = Isaac.Spawn(FiendFolio.FF.DeadFlyOrbital.ID, FiendFolio.FF.DeadFlyOrbital.Var, 0, npc.Position, Vector.Zero, ent):ToNPC()
-                npc:Remove()
-                fly.Parent = ent
-                ent.Child = fly
-                fly:GetData().rotval = math.random(100)
-                fly:Update()
- 
-                d.init = true
-                return
-                -- Mod.Scheduler.Schedule(1, function(entity, NPC)
-                --     local fly = Isaac.Spawn(FiendFolio.FF.DeadFlyOrbital.ID, FiendFolio.FF.DeadFlyOrbital.Var, 0, NPC.Position, Vector.Zero, entity):ToNPC()
-                --     NPC:Remove()
-                --     fly.Parent = entity
-                --     fly:GetData().rotval = math.random(100)
-                --     fly:Update()
-                -- end, {ent, npc})
-                -- --d.init = true
+        if not d.BotB_init then
+            for _, ent in pairs(Isaac.FindInRadius(npc.Position, 30, EntityPartition.ENEMY)) do
+                if ent.Type ~= Entities.ETERNAL_FLY_DUMMY.TYPE and ent.Variant ~= Entities.ETERNAL_FLY_DUMMY.VARIANT then
+                    local fly = Isaac.Spawn(ffmod.FF.DeadFlyOrbital.ID, ffmod.FF.DeadFlyOrbital.Var, 0, npc.Position, Vector.Zero, ent)
+                    npc:Remove()
+                    fly.Parent = ent
+                    fly:Update()
+
+                    d.BotB_init = true
+                    return
+                end
             end
         end
     end
 end
-Mod:AddPriorityCallback(ModCallbacks.MC_NPC_UPDATE, CallbackPriority.LATE, ETERNAL_FLY.NPCUpdate, Entities.ETERNAL_FLY_DUMMY.TYPE)
+Mod:AddCallback(ModCallbacks.MC_NPC_UPDATE, ETERNAL_FLY.NPCUpdate, Entities.ETERNAL_FLY_DUMMY.TYPE)
