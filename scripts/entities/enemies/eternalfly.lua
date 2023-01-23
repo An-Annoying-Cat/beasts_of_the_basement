455e186b8b84af3264bc194618be35662d2438b2
An-Annoying-Cat <57719091+An-Annoying-Cat@users.noreply.github.com>
2023-01-23 13:57:02 -0600

    a couple eternal fly rooms

--- scripts/entities/enemies/eternalfly.lua
+++ scripts/entities/enemies/eternalfly.lua
@@ -7,12 +7,11 @@ function ETERNAL_FLY:NPCUpdate(npc)
     local d = npc:GetData()
     if npc.Variant == Entities.ETERNAL_FLY_DUMMY.VARIANT then
         if not d.BotB_init then
-            for _, ent in pairs(Isaac.FindInRadius(npc.Position, 30, EntityPartition.ENEMY)) do
+            for _, ent in pairs(Isaac.FindInRadius(npc.Position, 35, EntityPartition.ENEMY)) do
                 if ent.Type ~= Entities.ETERNAL_FLY_DUMMY.TYPE and ent.Variant ~= Entities.ETERNAL_FLY_DUMMY.VARIANT then
                     local fly = Isaac.Spawn(ffmod.FF.DeadFlyOrbital.ID, ffmod.FF.DeadFlyOrbital.Var, 0, npc.Position, Vector.Zero, ent)
                     npc:Remove()
                     fly.Parent = ent
-                    fly:Update()
 
                     d.BotB_init = true
                     return
