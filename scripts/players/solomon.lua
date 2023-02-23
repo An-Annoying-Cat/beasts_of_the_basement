755c23f17fc01c2f64e616a6cb698787ff6fdf5e
Bionic-Dog <45298564+Bionic-Dog@users.noreply.github.com>
2023-02-23 16:45:48 -0500

    Update the character menu anm2

--- scripts/players/solomon.lua
+++ scripts/players/solomon.lua
@@ -117,8 +117,8 @@ function Solomon:friendlyEnemyDefenseBuff(entity,amt,flags,_,_)
   if isSolomonHere then
     if EntityRef(entity).IsFriendly == true then
       local actualDamage = amt
-      if flags & DamageFlag.DAMAGE_FIRE ~= 0 or flags & DamageFlag.DAMAGE_EXPLOSION ~= 0 then
-        actualDamage = amt*0.2
+      if flags & DamageFlag.DAMAGE_FIRE ~= 0 or flags & DamageFlag.DAMAGE_EXPLOSION ~= 0 or flags & DamageFlag.DAMAGE_SPIKES ~= 0 then
+        actualDamage = amt*0
         return actualDamage
       else
         actualDamage = amt*0.25
